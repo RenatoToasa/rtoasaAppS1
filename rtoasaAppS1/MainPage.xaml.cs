a18ed226d6bab6f8d23f0a56a5563e18764ba52d
Renato Toasa <renato_toasa@hotmail.com>
2021-10-18 16:44:35 -0500

    Add project files.

---
+++ rtoasaAppS1/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace rtoasaAppS1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSaludo_Clicked(object sender, EventArgs e)
+        {
+            //Almacenando en una variable, lo que el usuario ingresa en la caja de texto
+            string texto = txtSaludo.Text;
+            string mensaje = "Hola " + texto;
+            //Alerta para el usuario con XAMARIN
+            DisplayAlert("Mensaje de bienvenida", mensaje, "OK");
+
+        }
+    }
+}
